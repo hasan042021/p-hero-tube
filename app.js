0e3f2c291fdb82f79389496c8a19b3d744868468
Hasan <hasan.dev04@gmail.com>
2024-08-30 11:46:58 +0600

    question answers are added

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
+// states
 let category_data = null;
 let cur_category = "All";
 let cur_category_id = 1000;
 let videos = [];
 
+// global Dom elements
+const catContainer = document.getElementById("cat-container");
+const sortBtn = document.getElementById("sort-btn");
+
+// Get the  category data
 function get_categories() {
   fetch(`https://openapi.programming-hero.com/api/videos/categories`)
     .then((res) => res.json())
     .then((categories) => {
-      displayData(categories);
+      displayCategories(categories);
       category_data = categories;
     })
     .catch((err) => console.log(err));
 }
 
+// get all the videos by category
 function get_videos(categoryId) {
   fetch(
     `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
@@ -25,6 +32,7 @@ function get_videos(categoryId) {
     .catch((err) => console.log(err));
 }
 
+// utility functions
 function secondsToHoursMinutes(seconds) {
   const hours = Math.floor(seconds / 3600);
   const minutes = Math.floor((seconds % 3600) / 60);
@@ -36,50 +44,49 @@ function stringToNumber(str) {
 }
 
 function sortByViews(data) {
-  console.log("from here", data);
   const sortedData = [...data.data];
-  console.log("before", sortedData);
   sortedData.sort((a, b) => {
-    // Extract the view counts as numbers
     const viewA = stringToNumber(a?.others?.views);
     const viewB = stringToNumber(b?.others?.views);
 
     return viewB - viewA;
   });
-  console.log("after", sortedData);
-
   return sortedData;
 }
 
-const displayData = (categories) => {
-  console.log(categories);
-  const categoryContainer = document.querySelector("#cat-container");
-  categoryContainer.innerHTML = "";
+// Display categories
+const displayCategories = (categories) => {
+  catContainer.innerHTML = "";
   categories.data.map((category) => {
     console.log(category);
+    // create a button element for the category
     const button = document.createElement("button");
+    // add class for the category
     if (cur_category == category.category)
       button.classList.add("btn", "btn-danger", "btn-sm");
     else button.classList.add("btn", "btn-secondary", "btn-sm");
+    // set the button content
     button.textContent = category.category;
-    categoryContainer.appendChild(button);
+    // append the button
+    catContainer.appendChild(button);
   });
   get_videos(cur_category_id);
 };
 
-const sortBtn = document.getElementById("sort-btn");
+// display videos
 const display_videos = (videos) => {
-  console.log(videos.data);
   const videosContainer = document.getElementById("video-llist");
   const errorContainer = document.getElementById("error");
-
+  // check if there are any videos
   if (videos?.data?.length <= 0) {
     sortBtn.style.display = "none";
     videosContainer.innerHTML = "";
-    errorContainer.innerHTML = `<div class="d-flex flex-column align-items-center justify-content-center">
+    errorContainer.innerHTML = `
+      <div class="d-flex flex-column align-items-center justify-content-center">
          <img src="images/Icon.png" alt="" />
         <h2 class="w-md-50">Oops!! Sorry, There is no content here</h2>
-      </div>`;
+      </div>
+      `;
   } else {
     sortBtn.style.display = "inline-block";
     videosContainer.innerHTML = "";
@@ -87,6 +94,7 @@ const display_videos = (videos) => {
     videos.data.forEach((d) => {
       const videoItem = document.createElement("div");
       videoItem.classList.add("col");
+      // convert time
       let time = null;
       if (d.others.posted_date) {
         time = secondsToHoursMinutes(d.others.posted_date);
@@ -97,19 +105,18 @@ const display_videos = (videos) => {
             <div class="position-relative overflow-hidden">
             <img src="${
               d.thumbnail
-            }" class="card-img-top" style="height:120px;object-fit:cover; border-radius:5px;" alt="..." />
+            }" class="card-img-top thumbnail" style="height:120px;object-fit:cover; border-radius:5px;" alt="..." />
             ${
               d.others.posted_date
                 ? ` <p class="bg-black text-white position-absolute bottom-0 end-0 m-2 px-1" style="font-size:10px">${time.hours} hours ${time.minutes}mins ago</p>`
                 : ""
             }
-            
             </div>
             <div class="card-body g-0 py-1 px-0 d-flex justify-content-between">
               <div class="col-2 d-flex">
                 <img src="${
                   d.authors[0].profile_picture
-                }" style="object-fit:cover; width:30px;height:30px; border-radius:50%;"  alt="" />
+                }" style="object-fit:cover; width:30px;height:30px; border-radius:50%;"  alt="" class="author-img" />
               </div>
               <div class="col-10">
                 <p style="font-weight:500;font-size:14px" class="card-title small mb-0">${
@@ -138,8 +145,7 @@ const display_videos = (videos) => {
   }
 };
 
-const catContainer = document.getElementById("cat-container");
-
+// change data based on category
 catContainer.addEventListener("click", (e) => {
   if (e.target.tagName === "BUTTON") {
     const clickedButton = e.target;
@@ -148,16 +154,16 @@ catContainer.addEventListener("click", (e) => {
       (cat) => cat.category === buttonText
     ).category_id;
     cur_category = buttonText;
-    displayData(category_data);
+    displayCategories(category_data);
   }
 });
 
+// sort functionality by category
 sortBtn.addEventListener("click", (e) => {
   const result = sortByViews(videos);
   const sortedVideos = {
     data: result,
   };
-  console.log("sorted 157", sortedVideos);
   display_videos(sortedVideos);
 });
 
